bc86e1b8792c1cb6346403e42886aeefe7898d26
Randall LaPoint, Jr. <rplapointjr@gmail.com>
2025-01-01 20:36:11 -0600

    pricing card optimization for larger devices

--- app/pricing/page.js
+++ app/pricing/page.js
@@ -4,7 +4,7 @@ import { plans } from "@/lib/constants";
 
 const PricingPlans = () => {
   return (
-    <div className="min-h-screen p-6 max-w-2xl mx-auto grid sm:grid-cols-2 gap-6 md:grid-cols-3">
+    <div className="p-6 max-w-4xl mx-auto grid sm:grid-cols-2 gap-6 md:grid-cols-3">
       {plans.map((plan, index) => (
         <PricingPlanCard key={index} {...plan} />
       ))}
